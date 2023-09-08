b9575f1452992c6befc0d9fc1f3d810ba37df4b4
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-09-08 14:44:59 -0300

    Função de criaCliente() criada; Chamada dentro do clienteService

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,10 +1,35 @@
 const listaClientes = async () => {
     const resposta = await fetch(`http://localhost:3000/profile`)
     const data = await resposta.json()
-    console.log(data)
+
     return data
 }
 
+const criaCliente = async (nome, email) => {
+    try {
+        const resposta = await fetch(`http://localhost:3000/profile`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                nome: nome,
+                email: email
+            })
+        })
+
+        if (resposta.ok) {
+            return await resposta.json();
+        } else {
+            throw new Error('Erro na criação do cliente');
+        }
+    }
+    catch (error) {
+        console.error('Erro ao criar o cliente:', error);
+        throw error;
+    }
+}
+
 export const clienteService = {
-    listaClientes
+    listaClientes, criaCliente
 }
