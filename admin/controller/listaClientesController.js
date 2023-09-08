0f4b06bf5efdaadd56efbad3776deda930d6e97c
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-09-08 11:25:49 -0300

    Criação de controller para chamada de funçoes

---
+++ admin/controller/listaClientesController.js
@@ -0,0 +1,32 @@
+import { clienteService } from "../service/cliente-service"
+
+const criaNovaLinha = (nome, email) => {
+    const linhaNovoCliente = document.createElement('tr')
+    const conteudo = `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        </ul>
+    </td>`
+    linhaNovoCliente.innerHTML = conteudo
+    return linhaNovoCliente
+}
+
+const tabela = document.querySelector('[data-tabela]')
+
+clienteService.listaClientes()
+.then(data => {
+    if (Array.isArray(data)) {
+        data.forEach(e => {
+            tabela.appendChild(criaNovaLinha(e.nome, e.email))
+        })
+    } else {
+        console.error("Os dados retornados não são uma matriz.")
+    }
+})
+.catch(error => {
+    console.error("Erro ao obter a lista de clientes: ", error)
+})
