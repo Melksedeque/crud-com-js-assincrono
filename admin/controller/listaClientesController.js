e635dfcf724cb8409d87663d8fe39e64be987b3f
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-09-08 15:24:15 -0300

    Criação de função para deletar cliente da lista

--- admin/controller/listaClientesController.js
+++ admin/controller/listaClientesController.js
@@ -1,6 +1,6 @@
 import { clienteService } from "../service/cliente-service.js"
 
-const criaNovaLinha = (nome, email) => {
+const criaNovaLinha = (nome, email, id) => {
     const linhaNovoCliente = document.createElement('tr')
     const conteudo = `
     <td class="td" data-td>${nome}</td>
@@ -11,17 +11,30 @@ const criaNovaLinha = (nome, email) => {
             <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
         </ul>
     </td>`
+
     linhaNovoCliente.innerHTML = conteudo
+    linhaNovoCliente.dataset.id = id
+
     return linhaNovoCliente
 }
 
 const tabela = document.querySelector('[data-tabela]')
 
+tabela.addEventListener('click', (e) => {
+    let btnDeletar = e.target.className === 'botao-simples botao-simples--excluir'
+
+    if(btnDeletar) {
+        const linhaCliente = e.target.closest('[data-id]')
+        let id = linhaCliente.dataset.id
+        clienteService.deletaCliente(id)
+    }
+})
+
 clienteService.listaClientes()
 .then(data => {
     if (Array.isArray(data)) {
         data.forEach(e => {
-            tabela.appendChild(criaNovaLinha(e.nome, e.email))
+            tabela.appendChild(criaNovaLinha(e.nome, e.email, e.id))
         })
     } else {
         console.error("Os dados retornados não são uma matriz.")
