2c712aaed5372c7c1d991b069453134ca221e27c
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-09-09 18:03:56 -0300

    Função de fadeOut finalizada para animação de saída

--- admin/service/functions.js
+++ admin/service/functions.js
@@ -1,12 +1,11 @@
 const fadeOut = (element, duration) => {
-    element.style.transition = `opacity ${duration / 1000}s ease-in-out`;
-    element.style.opacity = 0;
-    
-    setTimeout(() => {
-        element.style.display = 'none';
-    }, duration);
-}
+    element.classList.add('fadeOut');
 
+    setTimeout(() => {
+        element.style.display = 'none'
+        element.remove()
+    }, duration)
+}
 
 export const functions = {
     fadeOut
