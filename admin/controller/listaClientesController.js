3594c5bdc213f407731d983dbcf8fe00074e8fc6
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-09-10 10:41:39 -0300

    Criação de função async render com try e catch para listagem de clientes

--- admin/controller/listaClientesController.js
+++ admin/controller/listaClientesController.js
@@ -8,7 +8,7 @@ const criaNovaLinha = (nome, email, id) => {
     <td>${email}</td>
     <td>
         <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+            <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
             <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
         </ul>
     </td>`
@@ -21,30 +21,34 @@ const criaNovaLinha = (nome, email, id) => {
 
 const tabela = document.querySelector('[data-tabela]')
 
-tabela.addEventListener('click', (e) => {
+tabela.addEventListener('click', async (e) => {
     let btnDeletar = e.target.className === 'botao-simples botao-simples--excluir'
 
     if(btnDeletar) {
-        const linhaCliente = e.target.closest('[data-id]')
-        let id = linhaCliente.dataset.id
-        clienteService.deletaCliente(id)
-        .then(() => {
+        try {
+            const linhaCliente = e.target.closest('[data-id]')
+            let id = linhaCliente.dataset.id
+            await clienteService.deletaCliente(id)
             functions.fadeOut(linhaCliente, 500)
-            console.log(linhaCliente)
-        })
+        }
+        catch (error) {
+            console.log(error)
+            window.location.href = '../telas/erro.html'
+        }
     }
 })
 
-clienteService.listaClientes()
-.then(data => {
-    if (Array.isArray(data)) {
-        data.forEach(e => {
+const render = async () => {
+    try {
+        const listaClientes = await clienteService.listaClientes()
+        listaClientes.forEach(e => {
             tabela.appendChild(criaNovaLinha(e.nome, e.email, e.id))
         })
-    } else {
-        console.error("Os dados retornados não são uma matriz.")
     }
-})
-.catch(error => {
-    console.error("Erro ao obter a lista de clientes: ", error)
-})
+    catch (error) {
+        console.log(error)
+        window.location.href = '../telas/erro.html'
+    }
+}
+
+render()
