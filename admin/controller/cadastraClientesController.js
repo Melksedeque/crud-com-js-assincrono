0b598ed72d3403ecb1725cdc56b76ce3501ebf07
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-09-10 10:39:28 -0300

    Inclusão de try e catch para tratar erros no cadastro de clientes

--- admin/controller/cadastraClientesController.js
+++ admin/controller/cadastraClientesController.js
@@ -2,13 +2,17 @@ import { clienteService } from "../service/cliente-service.js"
 
 const formulario = document.querySelector('[data-form]')
 
-formulario.addEventListener('submit', (e) => {
+formulario.addEventListener('submit', async (e) => {
     e.preventDefault()
     const nome = e.target.querySelector('[data-nome]').value
     const email = e.target.querySelector('[data-email]').value
     
-    clienteService.criaCliente(nome, email)
-    .then(() => {
+    try {
+        await clienteService.criaCliente(nome, email)
         window.location.href = '../telas/cadastro_concluido.html'
-    })
+    }
+    catch (error) {
+        console.log(error)
+        window.location.href = '../telas/erro.html'
+    }
 })
